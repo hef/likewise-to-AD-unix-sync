d8b93ca4776cd543a037b84734e875aae83f7e3c
hef <hef@acm.cs.uic.edu>
2009-10-20 19:56:16 -0500

    kind of works now

--- update.pl
+++ update.pl
@@ -33,23 +33,63 @@ foreach my $line (@lines)
 sub ldapfinduser
 {
 	my ($username, $uid, $gid) = @_;
+	print "=========\n";
 	print "$username $uid $gid\n";
-	my $mesg = $ad->search(base =>"OU=ACMUsers,DC=acm,DC=cs", filter => "sAMAccountName=$username");
-	foreach my $entry ($mesg->entries)
+	my $results = $ad->search(base =>"OU=ACMUsers,DC=acm,DC=cs", filter => "sAMAccountName=$username");
+
+	foreach my $entry ($results->entries)
 	{
-		my $mesg = $ad->modify
-		(
-			$entry->dn(),
-			add => [ msSFU30NisDomain => 'acm' ],
-			replace => [ msSFU30Name => "$username" ],
-			replace => [ uidNumber => "$uid" ],
-			add => [ gidNumber => "$gid" ],
-			replace => [ unixHomeDirectory => "/home/$username" ],
-			replace => [ loginShell => "/bin/bash" ]
-		);
-		print "\n";
+		print $entry->get_value('gidNumber'), "\n";
+		print $entry->get_value('unixHomeDirectory'), "\n";
+		print $entry->get_value('LoginShell'), "\n";
+		print $entry->get_value('msSFU30Name'), "\n";
+		print $entry->get_value('msSFU30NisDomain'), "\n";
+		print $entry->get_value('uidNumber'), "\n";
+		
+		my $mesg;
+		$mesg = $ad->modify( $entry->dn(), add => { msSFU30NisDomain => "acm" } );
+		#LDAPerror($mesg);
+		$mesg = $ad->modify( $entry->dn(), add => { unixHomeDirectory => "/home/$username" } );
+		#LDAPerror($mesg);
+		$mesg = $ad->modify( $entry->dn(), add => { msSFU30Name => "$username" } );
+		#LDAPerror($mesg);
+		$mesg = $ad->modify( $entry->dn(), add => { gidNumber => "$gid" } );
+		#LDAPerror($mesg);
+		$mesg = $ad->modify( $entry->dn(), add => { uidNumber => "$uid" } );
+		#LDAPerror($mesg);
+
+
+
+		
+	#	add => [ msSFU30Name => "$username" ],
+	#		add => [ uidNumber => "$uid" ],
+	#		add => [ msSFU30UidNumber => "$uid" ],
+	#		add => [ gidNumber => "$gid" ],
+	#		add => [ unixHomeDirectory => "/home/$username" ],
+	#		add => [ loginShell => "/bin/bash" ]
+	
 	}
 }
 
 $ad->unbind();
 
+sub LDAPerror
+{
+	my ($mesg) = @_;
+	if( $mesg->code)
+	{
+		print "Return code: ", $mesg->code;
+		print "\tMessage: ", $mesg->error_name;
+		print " :",          $mesg->error_text;
+		print "MessageID: ", $mesg->mesg_id;
+		print "\tDN: ", $mesg->dn;
+
+		#---
+		# Programmer note:
+		#
+		#  "$mesg->error" DOESN'T work!!!
+		#
+		#print "\tMessage: ", $mesg->error;
+		#-----
+	}
+}
